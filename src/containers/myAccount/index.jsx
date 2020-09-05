1599d555d2650475a88a287cc59e6e05107e660a
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-09-05 20:49:45 +0100

    Tweaks

--- src/containers/myAccount/index.jsx
+++ src/containers/myAccount/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import withRedirectOnNotAuth from '../../hocs/withRedirectOnNotAuth'
 
-const MyAccountContainer = ({ auth }) => <div>Hello, {auth.uid}!</div>
+const MyAccountContainer = ({ auth }) =>{
+  return (
+    <div>Hello, {auth.displayName}!</div>
+  )
+}
 
 const mapStateToProps = ({ firebase: { auth } }) => ({
   auth
